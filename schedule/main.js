c5c0a65280bcd9d2bf5047cc2df1968e7ebcc5d7
koorya <puaktopblm@yandex.ru>
2016-10-28 01:32:21 +0700

    Update main.js

--- schedule/main.js
+++ schedule/main.js
@@ -55,7 +55,7 @@ window.onload = function main()
 	mon.add(Course.create(16*60 + 20, 1*60 + 35, SEMINAR,  sbp,  nsu(433),       kozhevnikov ));
 	mon.add(Course.create(18*60 + 10, 1*60 + 35, ASSIGN,  sbp,  nsu(433),       kozhevnikov ));
 	
-	tue.add(Course.create( 12*60 +  40, 1*60 + 35, LECTURE, ast,  nsu('БА'),     surdin      , -1));
+	tue.add(Course.create( 12*60 +  40, 1*60 + 35, LECTURE, ast,  nsu('БА'),     surdin      ));
 	tue.add(Course.create(14*60 + 30, 1*60 + 35, PRACTICE, sbp,  nsu('т.4213'),  tkachenko   , 1));
 	tue.add(Course.create(14*60 + 30, 1*60 + 35, LECTURE,  sbp,  nsu(324),       kozhevnikov , 0 ));
 	// tue.add(Course.create(14*60 + 30, 1*60 + 35, PRACTICE, sbp,  inp(324),       kozhevnikov ));
@@ -73,7 +73,7 @@ window.onload = function main()
 	thu.add(Course.create(15*60 + 30, 1*60 + 35, PRACTICE, det, inp(508),        kurilin     ));
 	
 	fri.add(Course.create(10*60 + 50, 1*60 + 35, SEMINAR,  sbp,  nsu(326),       kozhevnikov ));
-	fri.add(Course.create(12*60 + 40, 1*60 + 35, LECTURE, ast,  nsu('БА'),       surdin      , -1));
+	fri.add(Course.create(12*60 + 40, 1*60 + 35, LECTURE, ast,  nsu('БА'),       surdin      ));
 	fri.add(Course.create(14*60 + 30, 1*60 + 35, SEMINAR,  phil, nsu(316),       borisov     ));
 	
 	sat.add(Course.create( 9*60 +  0, 1*60 + 35, LECTURE,  phil, nsu('БА'),      tsyplakov   ));
