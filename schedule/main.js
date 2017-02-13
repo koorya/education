13af0d0f772a323f266376a300d4b02109954d6d
koorya <puaktopblm@yandex.ru>
2017-02-13 19:42:22 +0700

    Update main.js

--- schedule/main.js
+++ schedule/main.js
@@ -56,37 +56,21 @@ window.onload = function main()
 	mon.add(Course.create(15*60 + 25, 1*60 + 25, PRACTICE,  sport,  sk()			 ));
 	
 	tue.add(Course.create( 12*60 +  40, 1*60 + 25, LECTURE, ast,  nsu('БА'),     surdin      ));
-	tue.add(Course.create(14*60 + 30, 1*60 + 25, PRACTICE, sbp,  nsu('т.4213'),  tkachenko   , 1));
-	tue.add(Course.create(14*60 + 30, 1*60 + 25, LECTURE,  sbp,  nsu(324),       kozhevnikov , 0 ));
-	// tue.add(Course.create(14*60 + 30, 1*60 + 35, PRACTICE, sbp,  inp(324),       kozhevnikov ));
-	tue.add(Course.create(16*60 + 20, 1*60 + 25, SEMINAR,  mmp,  nsu(328),       belheeva    ));
-	tue.add(Course.create(18*60 + 10, 1*60 + 25, SEMINAR,  eng,  nsu(5218),      tihomirova  ));
 	
 	wed.add(Course.create(10*60 + 45, 1*60 + 25, LECTURE,  det,  inp('КЗал'),    zhulanov    ));
-	wed.add(Course.create(12*60 + 30, 1*60 + 25, PRACTICE, unix, inp(508),       rutman      ));
-	wed.add(Course.create(14*60 + 15, 1*60 + 25, LECTURE,  unix, inp(508),       irtegov     ));
-	wed.add(Course.create(16*60 + 00, 1*60 + 25, SEMINAR,  shel, inp(508),       irtegov     ));
 	
 	thu.add(Course.create( 9*60 +  0, 1*60 + 25, PRACTICE, net,  inp(508),       sosedkin    ));
-	thu.add(Course.create(10*60 + 50, 1*60 + 25, LECTURE,  net,  inp(508),       cheblakov   ));
-	thu.add(Course.create(12*60 + 40, 1*60 + 25, SEMINAR,  eng,  nsu(5263),      tihomirova  ));
-	thu.add(Course.create(15*60 + 30, 1*60 + 25, PRACTICE, det, inp(508),        kurilin     ));
 
 	fri.add(Course.create(10*60 + 50, 1*60 + 25, SEMINAR,  sbp,  nsu(326),       kozhevnikov ));
-	fri.add(Course.create(12*60 + 40, 1*60 + 25, LECTURE, ast,  nsu('БА'),       surdin      ));
-	fri.add(Course.create(14*60 + 30, 1*60 + 25, SEMINAR,  phil, nsu(316),       borisov     ));
 	
 	sat.add(Course.create( 9*60 +  0, 1*60 + 25, LECTURE,  phil, nsu('БА'),      tsyplakov   ));
-	sat.add(Course.create(10*60 + 50, 1*60 + 25, LECTURE,  mmp,  nsu('315'),     abasheeva   ));
-	sat.add(Course.create(14*60 + 30, 1*60 + 25, SEMINAR,  mmp,  nsu('321а'),     belheeva    ));
-	sat.add(Course.create(16*60 + 20, 1*60 + 25, ASSIGN,  mmp,  nsu('247'),     belheeva    ));
 	
 	week.add(mon);
-	week.add(tue);
-	week.add(wed);
-	week.add(thu);
-	week.add(fri);
-	week.add(sat);
+//	week.add(tue);
+//	week.add(wed);
+//	week.add(thu);
+//	week.add(fri);
+//	week.add(sat);
 	
 	frame.add(week);
 	
